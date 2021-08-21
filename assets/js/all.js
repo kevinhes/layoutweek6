a2cef6dfa2d22338cbc3845cc291469754639761
kevinhes <kevinhes1115@gmail.com>
2021-08-21 15:19:12 +0800

    Update 2021-08-21T07:19:10.188Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -10,10 +10,27 @@ var swiper = new Swiper('.swiper-container', {
 });
 var pageTitle = document.title;
 var banner = document.querySelector('.homepage-banner');
+var footer = document.querySelector('.footer');
+var catalog = document.querySelector('.catalog');
 
 if (pageTitle == "exhibinection") {
   banner.style.display = "none";
 } else if (pageTitle == "exhibinection-detail") {
   banner.style.display = "none";
+} else if (pageTitle == "checkout-confirm") {
+  banner.style.display = "none";
+  footer.style.display = "none";
+  catalog.style.display = "none";
+} else if (pageTitle == "checkout-submit") {
+  banner.style.display = "none";
+  footer.style.display = "none";
+  catalog.style.display = "none";
+} else if (pageTitle == "checkout-credit") {
+  banner.style.display = "none";
+  footer.style.display = "none";
+  catalog.style.display = "none";
+} else if (pageTitle == "checkout-finish") {
+  banner.style.display = "none";
+  footer.style.display = "none";
 }
 //# sourceMappingURL=all.js.map
