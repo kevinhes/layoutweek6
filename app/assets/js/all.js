41c886f3ecbbc125347c2e720f7a554e7a6ce4a3
kevinhes <kevinhes1115@gmail.com>
2021-08-21 15:21:05 +0800

    final

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,9 +11,26 @@ const swiper = new Swiper('.swiper-container', {
 
 let pageTitle = document.title
 let banner = document.querySelector('.homepage-banner')
+let footer = document.querySelector('.footer')
+let catalog = document.querySelector('.catalog')
 
 if(pageTitle =="exhibinection") {
   banner.style.display="none"
 }else if(pageTitle =="exhibinection-detail"){
   banner.style.display="none"
+}else if(pageTitle =="checkout-confirm") {
+  banner.style.display="none"
+  footer.style.display="none"
+  catalog.style.display="none"
+}else if(pageTitle =="checkout-submit"){
+  banner.style.display="none"
+  footer.style.display="none"
+  catalog.style.display="none"
+}else if(pageTitle =="checkout-credit"){
+  banner.style.display="none"
+  footer.style.display="none"
+  catalog.style.display="none"
+}else if(pageTitle =="checkout-finish"){
+  banner.style.display="none"
+  footer.style.display="none"
 }
